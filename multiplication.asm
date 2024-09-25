8bfaddead7978fa1eadeb1aa0dd33448b1a9c6bd
eliThomass <ecthomas05@gmail.com>
2024-09-24 21:57:10 -0700

    added comments for multiplication.asm

--- multiplication.asm
+++ multiplication.asm
@@ -1,16 +1,16 @@
 section .data
-	EXIT equ 60
-	EXIT_SUCCESS equ 0
-	num1 dd 300000
-	num2 dd 400000
-	product dq 0
+	EXIT 			equ 	60
+	EXIT_SUCCESS 	equ 	0
+	num1 			dd 		300000
+	num2 			dd 		400000
+	product 		dq		 0
 section .text
 	global _start
 _start:
-	mov eax, dword[num1]
-	mul dword[num2]
-	mov dword[product+4], edx
-	mov dword[product], eax
+	mov eax, dword[num1]		; add num1 to eax
+	mul dword[num2]				; multiply eax by num2, results in edx:eax
+	mov dword[product+4], edx	; add edx (top 32 bits of product) to product																										
+	mov dword[product], eax		; add eax (lower 32 bits of product) to product
 	
 	mov rax, EXIT
 	mov rdi, EXIT_SUCCESS
